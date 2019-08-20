e9ace81d48a8c3ef8f02651a530afb3e7b64ab86
Arthur Mariot <arthur.mariot@gmail.com>
2019-08-20 15:56:08 +0200

    done motherfucker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  scope :hr_managers do
+    get 'features', to: 'pages#features', as: 'features'
+    resources :employees, only: [:new, :create, :index]
+    resources :companies, only: [:edit, :update]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
