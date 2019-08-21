f60005c949da7a308ebffa37fc2b6e410a92fe5e
Arthur Mariot <arthur.mariot@gmail.com>
2019-08-21 17:46:18 +0200

    ugly

--- db/migrate/20190820132401_add_field_to_users.rb
+++ db/migrate/20190820132401_add_field_to_users.rb
@@ -2,24 +2,24 @@ class AddFieldToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :personal_mail, :string
-    add_column :users, :corporate_mail, :string
-    add_column :users, :phone_number, :string
-    add_column :users, :linkedin_url, :string
-    add_column :users, :slack_account, :string
-    add_column :users, :time_zone, :string
-    add_column :users, :onboarding_progress, :string
-    add_column :users, :location, :string
-    add_column :users, :job_title, :string
-    add_column :users, :department, :string
+    add_column :users, :personal_mail, :string, default: ""
+    add_column :users, :corporate_mail, :string, default: ""
+    add_column :users, :phone_number, :string, default: ""
+    add_column :users, :linkedin_url, :string, default: ""
+    add_column :users, :slack_account, :string, default: ""
+    add_column :users, :time_zone, :string, default: ""
+    add_column :users, :onboarding_progress, :string, default: ""
+    add_column :users, :location, :string, default: ""
+    add_column :users, :job_title, :string, default: ""
+    add_column :users, :department, :string, default: ""
     add_reference :users, :team, foreign_key: true
     add_reference :users, :hierarchy_rank, foreign_key: true
-    add_column :users, :contract_status, :string
-    add_column :users, :contract_pdf, :string
-    add_column :users, :avatar, :string
-    add_column :users, :hobby_1, :string
-    add_column :users, :hobby_2, :string
-    add_column :users, :hobby_3, :string
+    add_column :users, :contract_status, :string, default: ""
+    add_column :users, :contract_pdf, :string, default: ""
+    add_column :users, :avatar, :string, default: ""
+    add_column :users, :hobby_1, :string, default: ""
+    add_column :users, :hobby_2, :string, default: ""
+    add_column :users, :hobby_3, :string, default: ""
     add_reference :users, :company, foreign_key: true
     add_column :users, :rules_reglementation_pdf, :string
     add_column :users, :arrival_at, :string
