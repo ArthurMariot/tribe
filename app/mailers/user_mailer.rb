65fffb3e8d8293068f050b95246e520ce6aa7cb0
edouard uguen <uguen.edouard@gmail.com>
2019-08-22 21:21:37 +0200

    fix some bugs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = User.last
-    mail(to: @user.email, subject: 'Welcome to @user.company.name')
+    mail(to: @user.email, subject: 'Welcome to Zapier')
   end
 end
