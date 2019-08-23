5067f924a9c5b2e3acb7b36f356ccb52cd36acd7
edouard uguen <uguen.edouard@gmail.com>
2019-08-23 14:30:07 +0200

    improve welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = User.last
-    mail(to: @user.email, subject: 'Welcome to Zapier')
+    mail(to: @user.email, subject: 'Welcome to Zapier 🚀')
   end
 end
