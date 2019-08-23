ea25d2cc51256701830c48efdd0c5572d29a3de1
Arthur Mariot <arthur.mariot@gmail.com>
2019-08-23 17:39:02 +0200

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://wwww.jointribe.world" }
+  config.action_mailer.default_url_options = { host: "http://www.jointribe.world" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
