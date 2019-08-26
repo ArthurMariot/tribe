295d5ba6fe126a48564af1f0520216796ab0fa96
Arthur Mariot <arthur.mariot@gmail.com>
2019-08-26 11:39:28 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,26 +22,31 @@ class User < ApplicationRecord
   pg_search_scope :search_by_team,
   against: [:team],
   using: {
-      tsearch: { prefix: true }
-    }
-    mount_uploader :avatar, AvatarUploader
-    mount_uploader :contract_pdf, FilespdfUploader
-    mount_uploader :upload_contrat, FilespdfUploader
-    mount_uploader :rules_reglementation_pdf, FilespdfUploader
-    mount_uploader :upload_internal_rules, FilespdfUploader
+    tsearch: { prefix: true }
+  }
+  mount_uploader :avatar, AvatarUploader
+  mount_uploader :contract_pdf, FilespdfUploader
+  mount_uploader :upload_contrat, FilespdfUploader
+  mount_uploader :rules_reglementation_pdf, FilespdfUploader
+  mount_uploader :upload_internal_rules, FilespdfUploader
 
-    def progress
-      @score = 0
-      self.avatar != "" ? @score += 1 : @score
-      self.hobby_1 != "" ? @score += 1 : @score
-      self.hobby_2 != "" ? @score += 1 : @score
-      self.hobby_3 != "" ? @score += 1 : @score
-      self.location != "" ? @score += 1 : @score
-      self.description != "" ? @score += 1 : @score
-      self.linkedin_url != "" ? @score += 1 : @score
-      self.personal_mail != "" ? @score += 1 : @score
-      self.visit_pages2 != "" ? @score += 1 : @score
-      self.visit_pages1 != "" ? @score += 1 : @score
-      return @score
-    end
+  def progress
+    @score = 0
+    self.avatar != "" ? @score += 1 : @score
+    self.hobby_1 != "" ? @score += 1 : @score
+    self.hobby_2 != "" ? @score += 1 : @score
+    self.hobby_3 != "" ? @score += 1 : @score
+    self.location != "" ? @score += 1 : @score
+    self.description != "" ? @score += 1 : @score
+    self.linkedin_url != "" ? @score += 1 : @score
+    self.personal_mail != "" ? @score += 1 : @score
+    self.visit_pages2 != "" ? @score += 1 : @score
+    self.visit_pages1 != "" ? @score += 1 : @score
+    return @score
   end
+
+  def buddy_object
+    @buddy = User.find((self.buddy).to_i)
+    return @buddy
+  end
+end
