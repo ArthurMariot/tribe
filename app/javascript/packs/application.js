b28cb2230ea130b76e97f7abab4fe4e482dd0f6c
Ramzi SAIDANI <ramzi@ramzi.fr>
2019-08-26 12:24:43 -0700

    algolia in the new employee form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import "../plugins/flatpickr"
+import "../plugins/flatpickr";
+import "../plugins/init_autocomplete"
 
 initMapbox();
 
