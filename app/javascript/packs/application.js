80ebe18a5fe6b8faae25da19067c39afd7516903
edouard uguen <uguen.edouard@gmail.com>
2019-08-26 17:13:10 +0200

    added alert but doesn't work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,22 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "../plugins/flatpickr";
 import "../plugins/init_autocomplete"
 import "../plugins/option_picker"
+import Swal from 'sweetalert2';
+
+const new_employee = document.querySelector("#new-employee");
+if (new_employee) {
+ apply.addEventListener("click", (event) => {
+   Swal.fire({
+     type: 'success',
+     title: '<span style="color:#FFFFFF; text-shadow: 1.5px 1.5px #ff487e"> CONGRATS!<span>',
+     width: 600,
+     html: '<span style="color:#FFFFFF">Your application has been sent!<span>',
+     background: '#1800FF',
+     showConfirmButton: false,
+     timer: 2200
+   });
+ });
+}
 
 initMapbox();
+
