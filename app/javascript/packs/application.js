c693de2cc801d01dd23fbf4905870e12ddc8a7ac
Ramzi SAIDANI <ramzi@ramzi.fr>
2019-08-26 18:37:22 -0700

    select2 done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "../plugins/flatpickr";
-import "../plugins/init_autocomplete"
-import "../plugins/option_picker"
+import "../plugins/init_autocomplete";
+import "../plugins/option_picker";
+import { initSelect2 } from "../plugins/init_select2";
 
 initMapbox();
+initSelect2();
+
---
+++ app/javascript/plugins/init_select2.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'select2';
+
+const initSelect2 = () => {
+  $('.select2').select2(); // (~ document.querySelectorAll)
+};
+
+export { initSelect2 };
+
