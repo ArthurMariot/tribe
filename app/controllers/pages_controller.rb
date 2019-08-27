117bcd678eb1584f5222bfdaa266371228741af3
Arthur Mariot <arthur.mariot@gmail.com>
2019-08-27 19:06:51 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,19 @@ class PagesController < ApplicationController
   end
 
   def features
- end
+  end
 
- def welcome
- end
+  def welcome
+  end
 
- def progress
-   current_user.progress
- end
+  def progress
+    current_user.progress
+  end
 
- def buddy
-   current_user.visit_pages6 = true
-   current_user.save
- end
+  def buddy
+    current_user.visit_pages6 = true
+    current_user.save
+  end
 
   def user_team_members
     current_user.visit_pages4 = true
